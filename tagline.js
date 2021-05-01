a613b41c2cfbe1b17b26cf3fa2901b0e48e84165
Jacob Runyan <jmrunyan@ucsc.edu>
2021-04-30 23:47:22 -0700

    Add rotating tagline

--- tagline.js
+++ tagline.js
@@ -1,12 +1,32 @@
+var taglines = [
+    ["Performance Lives Here.", "http://www.whitney.rip"],
+    ["Four Nines? More Like Four Zeroes.", ""],
+    ["0.0009999% is still technically Four Nines.", ""],
+    ["It's Gooch.", ""],
+    ["PWS, Powered by DWS", "https://www.onlypriests.com"],
+    ["SportsCenter NOT Top 10", "https://www.youtube.com/watch?v=KZk1ncguwHo"]
+]
+
 function tagline() {
 	console.log("Generating today's tagline.")
-	
-	var taglines = [
-		"Performance Lives Here.",
-		"Four Nines? More Like Four Zeroes.",
-		"0.0009999% is still technically Four Nines.",
-		"It's Gooch."
-	]
-	var idx = Math.floor((new Date()).getTime()/86400)
-	document.getElementById("tagline").innerHTML = taglines[Math.floor(idx % 2)]
+    var idx = Math.floor((new Date()).getTime()/86400)
+    document.getElementById("tagline").innerHTML = taglines[Math.floor(idx % taglines.length)][0]
+    document.getElementById("tagline").href = taglines[Math.floor(idx % taglines.length)][1]
+    if(taglines[Math.floor(idx % taglines.length)][1] == ""){
+        document.getElementById("tagline").classList.add('inactiveLink')
+    }else{
+        document.getElementById("tagline").classList.remove('inactiveLink')
+    }
+}
+
+function randomTagline() {
+	console.log("Generating a random tagline.")
+    var idx = Math.floor(Math.random() * taglines.length)
+	document.getElementById("tagline").innerHTML = taglines[Math.floor(idx % taglines.length)][0]
+    document.getElementById("tagline").href = taglines[Math.floor(idx % taglines.length)][1]
+    if(taglines[Math.floor(idx % taglines.length)][1] == ""){
+        document.getElementById("tagline").classList.add('inactiveLink')
+    }else{
+        document.getElementById("tagline").classList.remove('inactiveLink')
+    }
 }
