042948c4b08aee115871e9cf74b5bb30550224a6
Jacob Runyan <jmrunyan@ucsc.edu>
2021-05-03 18:20:58 -0700

    fix time, add paypal, add new font for tagline.

--- tagline.js
+++ tagline.js
@@ -1,15 +1,17 @@
 var taglines = [
-    ["Performance Lives Here.", "http://www.whitney.rip"],
+    ["Performance Lives Here.", "//whitney.rip"],
     ["Four Nines? More Like Four Zeroes.", ""],
     ["0.0009999% is still technically Four Nines.", ""],
     ["It's Gooch.", ""],
     ["PWS, Powered by DWS", "https://www.onlypriests.com"],
     ["SportsCenter NOT Top 10", "https://www.youtube.com/watch?v=KZk1ncguwHo"]
+    ["click the logo", ""],
+    ["buy me real internet","https://www.paypal.me/runyanjake"]
 ]
 
 function tagline() {
 	console.log("Generating today's tagline.")
-    var idx = Math.floor((new Date()).getTime()/86400)
+    var idx = Math.floor((new Date()).getTime()/86400000)
     document.getElementById("tagline").innerHTML = taglines[Math.floor(idx % taglines.length)][0]
     document.getElementById("tagline").href = taglines[Math.floor(idx % taglines.length)][1]
     if(taglines[Math.floor(idx % taglines.length)][1] == ""){
