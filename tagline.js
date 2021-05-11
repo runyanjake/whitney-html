0d77c07e998f660cb65d4c2ed5acdad7de8937cf
Jacob Runyan <jmrunyan@ucsc.edu>
2021-05-11 00:56:27 -0700

    Squashed some bugs and made the styling nicer.

--- tagline.js
+++ tagline.js
@@ -1,19 +1,24 @@
 var taglines = [
-    ["Performance Lives Here.", "//whitney.rip"],
+    ["Performance Lives Here.", "//www.whitney.rip"],
     ["Four Nines? More Like Four Zeroes.", ""],
     ["0.0009999% is still technically Four Nines.", ""],
     ["It's Gooch.", ""],
     ["PWS, Powered by DWS", "https://www.onlypriests.com"],
-    ["SportsCenter NOT Top 10", "https://www.youtube.com/watch?v=KZk1ncguwHo"]
+    ["SportsCenter NOT Top 10", "https://www.youtube.com/watch?v=KZk1ncguwHo"],
     ["click the logo", ""],
     ["buy me real internet","https://www.paypal.me/runyanjake"]
 ]
 
-function tagline() {
+var lastTagline = -1
+
+window.onload = function tagline() {
 	console.log("Generating today's tagline.")
-    var idx = Math.floor((new Date()).getTime()/86400000)
-    document.getElementById("tagline").innerHTML = taglines[Math.floor(idx % taglines.length)][0]
-    document.getElementById("tagline").href = taglines[Math.floor(idx % taglines.length)][1]
+    var htmlElement = document.getElementById("tagline")
+    var day = Math.floor((new Date()).getTime()/86400000)
+    var idx = Number(Math.floor(day % taglines.length))
+    lastTagline = idx
+    htmlElement.innerHTML = taglines[Math.floor(idx % taglines.length)][0]
+    htmlElement.href = taglines[Math.floor(idx % taglines.length)][1]
     if(taglines[Math.floor(idx % taglines.length)][1] == ""){
         document.getElementById("tagline").classList.add('inactiveLink')
     }else{
@@ -23,7 +28,11 @@ function tagline() {
 
 function randomTagline() {
 	console.log("Generating a random tagline.")
-    var idx = Math.floor(Math.random() * taglines.length)
+    var idx = lastTagline;
+    while(idx == lastTagline){
+        idx = Math.floor(Math.random() * taglines.length)
+    }
+    lastTagline = idx;
 	document.getElementById("tagline").innerHTML = taglines[Math.floor(idx % taglines.length)][0]
     document.getElementById("tagline").href = taglines[Math.floor(idx % taglines.length)][1]
     if(taglines[Math.floor(idx % taglines.length)][1] == ""){
