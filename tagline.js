0b799b6f101e61a53b0f320850b065f8850d0c21
Jake Runyan <runyanjake@gmail.com>
2021-07-06 17:59:01 -0700

    Add premiere for taglines

--- tagline.js
+++ tagline.js
@@ -8,23 +8,35 @@ var taglines = [
     ["click the logo", ""],
     ["buy me real internet","https://www.paypal.com/donate?business=ZZPAPDLLM3BGN&item_name=Donation&currency_code=USD"],
     ["support the covid project!","https://www.paypal.com/donate?token=f3eoW8tTdovlcY7ocFQQJvHcZv-lt0Xa3YFZA22YQd4zATfsBb7PmHz_K5AYJuPp01Pz5wzTzRIli_KI"],
-    ["live on Twitch","https://www.twitch.tv/pz_aerial"]
+    ["live on Twitch","https://www.twitch.tv/pz_aerial"],
+    ["getting back on K", "https://www.youtube.com/watch?v=0C5fNFRLqwE"]
 ]
 
 var lastTagline = -1
+const premiereUntil = new Date('July 10, 2021 00:00:00')
 
 window.onload = function tagline() {
 	console.log("Generating today's tagline.")
     var htmlElement = document.getElementById("tagline")
-    var day = Math.floor((new Date()).getTime()/86400000)
-    var idx = Number(Math.floor(day % taglines.length))
-    lastTagline = idx
-    htmlElement.innerHTML = taglines[Math.floor(idx % taglines.length)][0]
-    htmlElement.href = taglines[Math.floor(idx % taglines.length)][1]
-    if(taglines[Math.floor(idx % taglines.length)][1] == ""){
-        document.getElementById("tagline").classList.add('inactiveLink')
-    }else{
-        document.getElementById("tagline").classList.remove('inactiveLink')
+    if(Date.now() < premiereUntil){
+        htmlElement.innerHTML = taglines[taglines.length-1][0]
+        htmlElement.href = taglines[taglines.length-1][1]
+        if(taglines[taglines.length-1][1] == ""){
+            document.getElementById("tagline").classList.add('inactiveLink')
+        }else{
+            document.getElementById("tagline").classList.remove('inactiveLink')
+        }
+    } else {
+        var day = Math.floor((new Date()).getTime()/86400000)
+        var idx = Number(Math.floor(day % taglines.length))
+        lastTagline = idx
+        htmlElement.innerHTML = taglines[Math.floor(idx % taglines.length)][0]
+        htmlElement.href = taglines[Math.floor(idx % taglines.length)][1]
+        if(taglines[Math.floor(idx % taglines.length)][1] == ""){
+            document.getElementById("tagline").classList.add('inactiveLink')
+        }else{
+            document.getElementById("tagline").classList.remove('inactiveLink')
+        }
     }
 }
 
